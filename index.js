bdd16d7643466e78ec5332cec7f203fc783d2071
jafamahon <126361011+jafamahon@users.noreply.github.com>
2024-05-04 15:42:33 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var randomNumber1String = randomNumber1.toString();
+var randomNumber2String = randomNumber2.toString();
+document.querySelector(".img1").src="images/dice"+randomNumber1String+".png";
+document.querySelector(".img2").src="images/dice"+randomNumber2String+".png";
+if(randomNumber1>randomNumber2) {
+    document.querySelector("h1").innerHTML="🚩 Player 1 Wins!"
+} else {
+    document.querySelector("h1").innerHTML="Player 2 Wins! 🚩"
+}
