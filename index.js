c783e8dfa916b6f35df743ee35d62e684d8a74e7
jafamahon <126361011+jafamahon@users.noreply.github.com>
2024-05-04 15:59:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ document.querySelector(".img1").src="images/dice"+randomNumber1String+".png";
 document.querySelector(".img2").src="images/dice"+randomNumber2String+".png";
 if(randomNumber1>randomNumber2) {
     document.querySelector("h1").innerHTML="🚩 Player 1 wins!"
-} else if {
+} 
+else if {
     document.querySelector("h1").innerHTML="Player 2 wins! 🚩"
-} else {
+} 
+else {
     document.querySelector("h1").innerHTML="It's a draw!"
 };
