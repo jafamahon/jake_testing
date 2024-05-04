47fc40eea2b718addce1b6c92785ec7353cbd626
jafamahon <126361011+jafamahon@users.noreply.github.com>
2024-05-04 16:01:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ var randomNumber1 = Math.floor(Math.random()*6) + 1;
 var randomNumber2 = Math.floor(Math.random()*6) + 1;
 var randomNumber1String = randomNumber1.toString();
 var randomNumber2String = randomNumber2.toString();
+
 document.querySelector(".img1").src="images/dice"+randomNumber1String+".png";
 document.querySelector(".img2").src="images/dice"+randomNumber2String+".png";
-if(randomNumber1>randomNumber2) {
+
+if(randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML="🚩 Player 1 wins!"
 } 
-else if {
+else if(randomNumber1 < randomNumber2) {
     document.querySelector("h1").innerHTML="Player 2 wins! 🚩"
 } 
 else {
